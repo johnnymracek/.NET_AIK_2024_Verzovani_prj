c2d6082f938734470631dedf97a7ffb4b5c99d33
Johnny_ <145134365+johnnymracek@users.noreply.github.com>
2024-02-04 05:45:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Verzovani
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Získání vstupu od uživatele
+            Console.WriteLine("Zadejte první číslo:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadejte druhé číslo:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Výpočet průměru
+            int prumer = (num1 + num2) / 2;
+
+            // Výsledek
+            Console.WriteLine("Průměr ze čísel {0} a {1} je: {2}", num1, num2, prumer);
+        }
+    }
+
+}
